11bec6889f5b420cbe4f22decf673f753bff3d00
Silvio <silvioej@gmail.com>
2020-11-23 20:27:07 -0300

    test page for example

---
+++ views/test2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		h1{
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+<h1><?= 'Welcome to '.$_SERVER['SERVER_NAME'] ?></h1>
+
+</body>
+</html>
