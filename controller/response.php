11ceacff3d5a22d7be424384f437ccad5973f6a5
Silvio <silvioej@gmail.com>
2020-11-26 13:16:53 -0300

    now Response can send variables to pages

--- controller/response.php
+++ controller/response.php
@@ -5,11 +5,16 @@
  */
 class Response
 {
-	public $values = [];
+	private $values = [];
 
-	public function set($index,$value)
+	public function set($index,$value = null)
 	{
-		$this->values[$index] = $value;
+		if ($value === null) {
+			$this->values += $index;
+		}else{
+			$this->values[$index] = $value;
+
+		}
 	}
 
 	public function send($content = ''){
@@ -18,16 +23,23 @@ class Response
 
 	public function sendFile($path='')
 	{
-		if (sizeof($this->$values) > 0) {
-			foreach ($variable as $key => $value) {
-				$file = file_get_contents($path);
-				$file = preg_replace('{{'.$key.'}}', $value, $file);
-				$this->send($file);
+		$file = file_get_contents($path);
+		if (sizeof($this->values) > 0) {
+			foreach ($this->values as $key => $value) {
+				if (is_callable($value)) {
+					$file = preg_replace('/\{\{'.$key.'\}\}/', $value(), $file);
+					
+				}else{
+					$file = preg_replace('/\{\{'.$key.'\}\}/', $value, $file);
+
+				}
 			}
+				$this->send($file);
+		}else{
+			require $path;
 		}
-		require $path;
 	}
 }
 
 
- ?>
+?>
