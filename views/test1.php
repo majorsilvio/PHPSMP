014b2d1ebe5ce28af0a95ab72663d2f47f3741de
Silvio <silvioej@gmail.com>
2020-11-26 13:22:08 -0300

    examples of call variables in pages

--- views/test1.php
+++ views/test1.php
@@ -10,7 +10,7 @@
 	</style>
 </head>
 <body>
-<h1><?= 'Welcome to '.$_SERVER['SERVER_NAME'] ?></h1>
-<h2><a href="/test2">Change page</a></h2>
+<h1>Welcome to {{project}}</h1>
+<h2><a href="/test2">Change page to {{test}}</a></h2>
 </body>
 </html>
